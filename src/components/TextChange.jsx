55c1dc2c347a3549421dbde43174d5f9f07671e6
Arvind <palshahab1290@gmail.com>
2026-03-12 01:29:02 +0530

    Add a surname

--- src/components/TextChange.jsx
+++ src/components/TextChange.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState,useEffect } from 'react'
 
 const TextChange = () => {
-    const text=["Hi,I Am Arvind "]
+    const text=["Hi,I Am Arvind Pal"]
     const[currentText,setcurrentText]=useState("");
     const[endValue,setendValue]=useState(true)
     const [isForward,setIsForward]=useState(true)
